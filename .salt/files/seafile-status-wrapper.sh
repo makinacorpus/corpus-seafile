1184dbfb6f9e1e62ea93423cb2aae8c9e5f04b25
Mathieu Le Marec - Pasquet <kiorky@cryptelium.net>
2015-05-05 13:53:07 +0200

    sd lsb

--- .salt/files/seafile-status-wrapper.sh
+++ .salt/files/seafile-status-wrapper.sh
@@ -1,4 +1,14 @@
 #!/usr/bin/env bash
+### BEGIN INIT INFO
+# Provides:          seafile-daemons
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Starts sd
+### END INIT INFO
+
+# 
 {% set cfg = salt['mc_project.get_configuration'](cfg) %}
 cd "{{cfg.data.cur_ver_dir}}"
 su "{{cfg.user}}" -c "./seafile-status.sh ${@}"
